f9e7a9835afe664780c80a6c5db0def5cb6a3de8
James Randall <james@accidentalfish.com>
2024-12-18 06:30:28 +0000

    Initial Commit

---
+++ Sprite Packer/Sprite_PackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sprite_PackerApp.swift
+//  Sprite Packer
+//
+//  Created by James Randall on 18/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Sprite_PackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sprite PackerTests/Sprite_PackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Sprite_PackerTests.swift
+//  Sprite PackerTests
+//
+//  Created by James Randall on 18/12/2024.
+//
+
+import Testing
+@testable import Sprite_Packer
+
+struct Sprite_PackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
